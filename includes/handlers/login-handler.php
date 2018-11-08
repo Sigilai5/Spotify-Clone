62396b8ec092b74c9239461ef88f5abaa52bd9ab
sigilai <briansigilai@gmail.com>
2018-11-08 23:15:48 +0300

    created validation function

--- includes/handlers/login-handler.php
+++ includes/handlers/login-handler.php
@@ -5,3 +5,12 @@
  * Date: 11/8/18
  * Time: 11:02 PM
  */
+
+if(isset($_POST['loginButton'])){
+    //Login button was pressed
+
+    echo strip_tags($_POST['loginUsername']);
+
+}
+
+?>
--- register.php
+++ register.php
@@ -1,50 +1,8 @@
 <?php
-//Sanitization
-function sanitizeFormUsername($inputText) {
-    $inputText = strip_tags($inputText);//remove html tags to prevent injections
-    $inputText = str_replace(" ","",$inputText);//remove empty spaces
-    return $inputText;
-    }
-
-function sanitizeFormString($inputText) {
-    $inputText = strip_tags($inputText);//remove html tags to prevent injections
-    $inputText = str_replace(" ","",$inputText);//remove empty spaces
-    $inputText = ucfirst(strtolower($inputText));//convert the first letter to uppercase and convert the reast to lower
-    return $inputText;
-}
-
-function sanitizeFormPassword($inputText){
-    $inputText = strip_tags($inputText);
-    return $inputText;
-}
-
-
-if(isset($_POST['loginButton'])){
-    //Login button was pressed
-
-    echo strip_tags($_POST['loginUsername']);
-
-}
-
-if(isset($_POST['registerButton'])){
-    //Register button was pressed
-
-    $username = sanitizeFormUsername($_POST['username']);
-    $firstName = sanitizeFormString($_POST['firstName']);
-    $lastName = sanitizeFormString($_POST['lastName']);
-    $email = sanitizeFormString($_POST['email']);
-    $email2 = sanitizeFormString($_POST['email2']);
-
-    $password = sanitizeFormPassword($_POST['password']);
-    $password2 = sanitizeFormPassword($_POST['password2']);
-
-}
-
-
+include ("includes/handlers/register-handler.php");
+include ("includes/handlers/login-handler.php");
 
 ?>
-
-
 
 <html>
 <head>
