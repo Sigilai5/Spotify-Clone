d2a9b9ab180b2311f4be6e8636c45dd3352cf949
sigilai <briansigilai@gmail.com>
2018-11-10 14:55:11 +0300

    created Constants class

--- includes/handlers/Constants.php
+++ includes/handlers/Constants.php
@@ -5,3 +5,18 @@
  * Date: 11/10/18
  * Time: 2:50 PM
  */
+class  Constants {
+
+    public static $usernameCharacters = "Your Passwords don't match!";
+    public static $firstNameCharacters = "Your Passwords don't match!";
+    public static $lastNameCharacters  = "Your Passwords don't match!";
+    public static $emailsDoNotMatch = "Your Passwords don't match!";
+    public static $emailInvalid = "Your Passwords don't match!";
+    public static $passwordCharacters = "Your Passwords don't match!";
+    public static $passwordNotAlphanumeric = "Your Passwords don't match!";
+    public static $passwordDoNotMatch = "Your Passwords don't match!";
+
+}
+
+
+?>
