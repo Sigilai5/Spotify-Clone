341d282b5424de45f574c6710edf82690227b85a
sigilai <briansigilai@gmail.com>
2018-11-10 14:20:15 +0300

    validated register inputs

--- includes/classes/Account.php
+++ includes/classes/Account.php
@@ -1,7 +1,90 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: sigilai
- * Date: 11/8/18
- * Time: 11:24 PM
- */
+    class Account {
+
+        private $errorArray;
+
+        public function __construct(){
+            $this->errorArray = array();
+
+        }
+
+        public function register($un,$fn,$ln,$em,$em2,$pw,$pw2) {
+
+            $this->validateUsername($un);
+            $this->validateFirstName($fn);
+            $this->validateLastName($ln);
+            $this->validateEmails($em, $em2);
+            $this->validatePasswords($pw, $pw2);
+
+            if(empty($this->errorArray)==true){
+                //Insert into db
+                return true;
+            }
+            else {
+                return false;
+            }
+
+        }
+
+        private function validateUsername($un){
+
+            if(strlen($un) > 25 || strlen($un) < 5) {
+                array_push($this->errorArray,"Username must be between 5 to 25 characters");
+                return;
+            }
+
+            //TODO: check if username exists
+
+        }
+
+        private function validateFirstName($fn){
+            if(strlen($fn) > 25 || strlen($fn) < 2) {
+                array_push($this->errorArray,"First Name must be between 2 to 25 characters");
+                return;
+            }
+
+        }
+        private function validateLastName($ln){
+            if(strlen($ln) > 25 || strlen($ln) < 2) {
+                array_push($this->errorArray,"Last Name must be between 2 to 25 characters");
+                return;
+            }
+        }
+        private function validateEmails($em, $em2){
+            if($em != $em2) {
+                array_push($this->errorArray,"Your Emails don't match!");
+                return;
+            }
+            if(!filter_var($em,FILTER_VALIDATE_EMAIL)) {
+                array_push($this->errorArray,"Email is invalid!");
+                return;
+            }
+
+            //TODO: Check that username hasn't already been taken
+
+        }
+        private function validatePasswords($pw, $pw2){
+
+            if($pw != $pw2) {
+                array_push($this->errorArray,"Your Passwords don't match!");
+                return;
+            }
+            if(preg_match('/[^A-Za-z0-9]/',$pw)) {
+                array_push($this->errorArray,"Your Password can only contain numbers and letters");
+                return;
+            }
+
+            if(strlen($pw) > 30 || strlen($pw) < 5) {
+                array_push($this->errorArray,"Your password should contain 5 and 30 characters");
+                return;
+            }
+
+
+        }
+
+
+
+    }
+
+
+?>
--- includes/handlers/register-handler.php
+++ includes/handlers/register-handler.php
@@ -18,22 +18,7 @@ function sanitizeFormPassword($inputText){
     return $inputText;
 }
 
-function validateUsername($un){
 
-}
-
-function validateFirstName($fn){
-
-}
-function validateLastName($ln){
-
-}
-function validateEmails($em, $em2){
-
-}
-function validatePasswords($pw, $pw2){
-
-}
 
 if(isset($_POST['registerButton'])){
     //Register button was pressed
@@ -46,11 +31,11 @@ if(isset($_POST['registerButton'])){
     $password = sanitizeFormPassword($_POST['password']);
     $password2 = sanitizeFormPassword($_POST['password2']);
 
-    validateUsername($username);
-    validateFirstName($firstName);
-    validateLastName($lastName);
-    validateEmails($email, $email2);
-    validatePasswords($password, $password2);
+    $wasSuccessful = $account->register($username,$firstName,$lastName,$email,$email2,$password,$password2);
+
+    if ($wasSuccessful == true) {
+        header("Location: index.php");
+    }
 
 }
 
