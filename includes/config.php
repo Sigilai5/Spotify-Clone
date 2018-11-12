de43841b2628c0ce60ea55eb3d7d8291c2a0bf09
sigilai <briansigilai@gmail.com>
2018-11-12 12:39:42 +0300

    created images directory

--- includes/config.php
+++ includes/config.php
@@ -10,7 +10,7 @@ ob_start();
 
 $timezone = date_default_timezone_set("Africa/Nairobi");
 
-$con = mysqli_connect("localhost","root","","juk");
+$con = mysqli_connect("localhost","root","","jukwaa");
 
 if(mysqli_connect_errno()){
     echo "Failed to connect: " . mysqli_connect_error();
