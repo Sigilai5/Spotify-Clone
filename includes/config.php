a688aeff8ca5bf07c5e342cca36fe135c8aad42a
sigilai <briansigilai@gmail.com>
2018-11-12 12:09:38 +0300

    created connection between app and db

--- includes/config.php
+++ includes/config.php
@@ -5,3 +5,15 @@
  * Date: 11/12/18
  * Time: 12:04 PM
  */
+
+ob_start();
+
+$timezone = date_default_timezone_set("Africa/Nairobi");
+
+$con = mysqli_connect("localhost","root","","juk");
+
+if(mysqli_connect_errno()){
+    echo "Failed to connect: " . mysqli_connect_error();
+}
+
+?>
