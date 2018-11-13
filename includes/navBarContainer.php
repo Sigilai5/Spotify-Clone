78a1455923f612446ac559552d55d60f4a7fa535
sigilai <briansigilai@gmail.com>
2018-11-13 12:35:28 +0300

    created nowPlayingBar.php

--- includes/navBarContainer.php
+++ includes/navBarContainer.php
@@ -1,7 +1,35 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: sigilai
- * Date: 11/13/18
- * Time: 12:33 PM
- */
+<div id="navBarContainer">
+    <nav class="navBar">
+
+        <a href="index.php" class="logo">
+            <img src="assets/images/jukwaa.jpg">
+        </a>
+
+        <div class="group">
+
+            <div class="navItem">
+                <a href="search.php" class="navItemLink">Search
+                    <img src="assets/images/icons/search.png" class="icon" alt="Search">
+                </a>
+            </div>
+
+        </div>
+
+        <div class="group">
+
+            <div class="navItem">
+                <a href="browse.php" class="navItemLink">Browse</a>
+            </div>
+            <div class="navItem">
+                <a href="yourMusic.php" class="navItemLink">Your Music</a>
+            </div>
+            <div class="navItem">
+                <a href="profile.php" class="navItemLink">Brian Sigilai</a>
+            </div>
+
+
+        </div>
+
+    </nav>
+
+</div>
