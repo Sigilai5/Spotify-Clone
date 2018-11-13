602fd332cad696d57cab8c1a19b255f8d559676b
sigilai <briansigilai@gmail.com>
2018-11-13 18:20:39 +0300

    created Song.php file

--- album.php
+++ album.php
@@ -14,15 +14,33 @@ else {
     header("Location: index.php");
 }
 
-$albumQuery = mysqli_query($con, "SELECT * FROM albums WHERE id='$albumId'");
-$album = mysqli_fetch_array($albumQuery);
+//$albumQuery = mysqli_query($con, "SELECT * FROM albums WHERE id='$albumId'");
+//$album = mysqli_fetch_array($albumQuery);
 
-$artist = new Artist($con, $album['artist']);
+$album = new Album($con, $albumId);
+
+$artist = $album->getArtist();
 
 
-echo $album['title'] . "<br>";
-echo $artist->getName();
 
 ?>
+
+<div class="entityInfo">
+
+    <div class="leftSection">
+
+        <img src="<?php echo $album->getArtworkPath(); ?>">
+
+    </div>
+    <div class="rightSection">
+        <h2><?php echo $album->getAlbumId(); ?></h2>
+        <h2><?php echo $album->getTitle(); ?></h2>
+        <p>By <?php echo $artist->getname(); ?></p>
+        <p><?php echo $album->getNumberOfSongs(); ?> songs</p>
+    </div>
+
+
+</div>
+
 
 <?php include ("includes/footer.php"); ?>
--- includes/classes/Album.php
+++ includes/classes/Album.php
@@ -1,7 +1,49 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: sigilai
- * Date: 11/13/18
- * Time: 4:05 PM
- */
+class Album {
+
+    private $con;
+    private $id;
+    private $title;
+    private $artistId;
+    private $genre;
+    private $artworkPath;
+
+    public function __construct($con,$id){
+        $this->con = $con;
+        $this->id = $id;
+
+        $query = mysqli_query($this->con,"SELECT * FROM albums WHERE id='$this->id'");
+        $album = mysqli_fetch_array($query);
+
+        $this->albumId = $album['id'];
+        $this->title = $album['title'];
+        $this->artistId = $album['artist'];
+        $this->genre = $album['genre'];
+        $this->artworkPath = $album['artworkPath'];
+
+    }
+
+    public function getAlbumId(){
+        return $this->albumId;
+    }
+    public function getTitle(){
+     return $this->title;
+    }
+    public function getArtist(){
+        return new Artist($this->con, $this->artistId);
+    }
+    public function getGenre(){
+        return $this->genre;
+    }
+    public function getArtworkPath(){
+        return $this->artworkPath;
+    }
+    public function getNumberOfSongs() {
+        $query = mysqli_query($this->con, "SELECT * FROM Songs WHERE album='$this->id'"); //SELECT COUNT(*) FROM Songs WHERE albums='7'
+        return mysqli_num_rows($query);
+    }
+
+}
+
+
+?>
