d2004e1cdbf5801c2c8578d92e15fa0a2f5f4a7e
sigilai <briansigilai@gmail.com>
2018-11-15 23:08:08 +0300

    created Playlist.php file

--- includes/classes/User.php
+++ includes/classes/User.php
@@ -5,3 +5,22 @@
  * Date: 11/15/18
  * Time: 10:02 PM
  */
+
+class User{
+
+    private $con;
+    private $username;
+
+    public function __construct($con , $username){
+        $this->con = $con;
+        $this->username = $username;
+    }
+
+    public function getUsername(){
+        return $this->username;
+    }
+
+}
+
+
+?>
